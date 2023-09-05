263ae98f250466f91ea87a55610e706beef81328
Definitely-Human <odest66@gmail.com>
2023-09-05 23:22:32 +0200

    Added restaturants module and add restaurant resolver.

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UserResolver } from 'src/users/users.resolver';
+import { UserService } from 'src/users/users.service';
+import { RestaurantResolver } from './restaurants.resolver';
+import { RestaurantService } from './restaurants.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
+import { Category } from './entities/category.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
+  providers: [RestaurantResolver, RestaurantService],
+})
 export class RestaurantsModule {}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import {
+  CreateRestaurantInput,
+  CreateRestaurantOutput,
+} from './dtos/create-resturant.dto';
+import { RestaurantService } from './restaurants.service';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Mutation((returns) => CreateRestaurantOutput)
+  async createRestaurant(
+    @AuthUser() authUser: User,
+    @Args('input') createRestaurantInput: CreateRestaurantInput,
+  ): Promise<CreateRestaurantOutput> {
+    return this.restaurantService.createRestaurant(
+      authUser,
+      createRestaurantInput,
+    );
+  }
+}
