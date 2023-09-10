cdbeee9d5da5e941713d0897a1581ea51ba129bd
Definitely-Human <odest66@gmail.com>
2023-09-11 00:27:13 +0200

    Added getOrders resolver.

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,10 +1,11 @@
-import { Args, ArgsType, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, ArgsType, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { Role } from 'src/auth/role.decorator';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Resolver((of) => Order)
 export class OrderResolver {
@@ -17,5 +18,14 @@ export class OrderResolver {
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.createOrder(customer, createOrderInput);
+  }
+
+  @Query((returns) => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return this.ordersService.getOrders(user, getOrdersInput);
   }
 }
